ad2adc56b0219c224bc4f518d1be75ef965ff279
lufuno <lufunodagada43@gmail.com>
2025-02-05 00:34:09 +0200

    changed the prompt for the AI in JavaScript

--- AI poem.js
+++ AI poem.js
@@ -11,17 +11,17 @@ function generatePoems(event) {
 
   let interactionInput = document.querySelector("#interaction");
   let APIkey = "b08b4acf0t6108d50e30fa0b396od66f";
-  let prompt = `User instructions are: Generate a English poem about ${interactionInput.value}`;
+  let prompt = `User instructions are: Generate information and instructions about ${interactionInput.value}`;
   let context =
-    "you are a romantic English poet that loves to write a poem about love and happiness. Your mission is to generate a short poem using basic HTML  by following the user instructions. Sign the poem with Lufuno.D.";
+    "you are a AI that provides information for users and instructions to use such as recipes. Your mission is to generate information and instructions using basic HTML  by following the user instructions. Add reference list of where you got the information and instructions for the information provided at the end.";
 
   let APIurl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${APIkey}`;
 
   let poemElement = document.querySelector("#poem");
   poemElement.classList.remove("hidden");
 
-  poemElement.innerHTML = `<div class="blink">⏳ Generating poem for ${interactionInput.value}.....</div>`;
-  console.log("Generation poem....");
+  poemElement.innerHTML = `<div class="blink">⏳ Generating for ${interactionInput.value}.....</div>`;
+  console.log("Generating....");
   console.log(`Prompt: ${prompt}`);
   console.log(` Context: ${context}`);
 
